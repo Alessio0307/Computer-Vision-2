83704c443c85022db8811ac13d2c920746e4dc41
LDA98 <87715021+LDA98@users.noreply.github.com>
2024-06-21 11:42:03 +0200

    Update EDSR.py

--- lib/models/EDSR.py
+++ lib/models/EDSR.py
@@ -1,42 +1,30 @@
 import math
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-class ResBlock(nn.Module):
-    def __init__(self, conv, n_feats, kernel_size, bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
-        super(ResBlock, self).__init__()
-        modules_body = []
-        for i in range(2):
-            modules_body.append(conv(n_feats, n_feats, kernel_size, bias=bias, padding=1))
-            if bn:
-                modules_body.append(nn.BatchNorm2d(n_feats))
-            if i == 0:
-                modules_body.append(act)
-        self.body = nn.Sequential(*modules_body)
-        self.res_scale = res_scale
+def default_conv(in_channels, out_channels, kernel_size, bias=True):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size//2), bias=bias)
 
-    def forward(self, x):
-        res = self.body(x).mul(self.res_scale)
-        res += x
-        return res
 
 class Upsampler(nn.Sequential):
-    def __init__(self, conv, scale, n_feats, bn=False, act=False, bias=True):
+    def __init__(self, conv, scale, n_feat, bn=False, act=False, bias=True):
+
         m = []
-        if (scale & (scale - 1)) == 0:    # scale = 2^n
+        if (scale & (scale - 1)) == 0:    # Is scale = 2^n?
             for _ in range(int(math.log(scale, 2))):
-                m.append(conv(n_feats, 4 * n_feats, 3, bias))
+                m.append(conv(n_feat, 4 * n_feat, 3, bias))
                 m.append(nn.PixelShuffle(2))
                 if bn:
-                    m.append(nn.BatchNorm2d(n_feats))
+                    m.append(nn.BatchNorm2d(n_feat))
                 if act:
                     m.append(act())
         elif scale == 3:
-            m.append(conv(n_feats, 9 * n_feats, 3, bias))
+            m.append(conv(n_feat, 9 * n_feat, 3, bias))
             m.append(nn.PixelShuffle(3))
             if bn:
-                m.append(nn.BatchNorm2d(n_feats))
+                m.append(nn.BatchNorm2d(n_feat))
             if act:
                 m.append(act())
         else:
@@ -44,22 +32,53 @@ class Upsampler(nn.Sequential):
 
         super(Upsampler, self).__init__(*m)
 
-class EDSR(nn.Module):
-    def __init__(self, num_channels=256, out_channels=3, scale=1, n_resblock=16, n_feats=64, kernel_size=3):
-        super(EDSR, self).__init__()
-        conv = nn.Conv2d
 
-        act = nn.ReLU(True)
+class ResBlock(nn.Module):
+    def __init__(self, conv, n_feat, kernel_size, bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
+        super(ResBlock, self).__init__()
+
+        m = []
+        for i in range(2):
+            m.append(conv(n_feat, n_feat, kernel_size, bias=bias))
+            if bn:
+                m.append(nn.BatchNorm2d(n_feat))
+            if i == 0:
+                m.append(act)
+
+        self.body = nn.Sequential(*m)
+        self.res_scale = res_scale
+
+    def forward(self, x):
+        res = self.body(x).mul(self.res_scale)
+        res += x
+
+        return res
+
+
+class EDSR(nn.Module):
+    def __init__(self, num_channels=3, input_channel=32, factor=2, width=64, depth=16, kernel_size=3, conv=default_conv):
+        super(EDSR, self).__init__()
+
+        n_resblock = depth
+        n_feats = width
+        kernel_size = kernel_size
+        scale = factor
+        act = nn.ReLU()
 
         # define head module
-        m_head = [conv(num_channels, n_feats, kernel_size, padding=1)]
+        m_head = [conv(input_channel, n_feats, kernel_size)]
 
         # define body module
-        m_body = [ResBlock(conv, n_feats, kernel_size, act=act, res_scale=1.) for _ in range(n_resblock)]
-        m_body.append(conv(n_feats, n_feats, kernel_size, padding=1))
+        m_body = [ResBlock(conv,
+                           n_feats,
+                           kernel_size,
+                           act=act,
+                           res_scale=1.) for _ in range(n_resblock)]
+        m_body.append(conv(n_feats, n_feats, kernel_size))
 
         # define tail module
-        m_tail = [conv(n_feats, out_channels, kernel_size, padding=1)]
+        m_tail = [Upsampler(conv, scale, n_feats, act=False),
+                  conv(n_feats, num_channels, kernel_size)]
 
         self.head = nn.Sequential(*m_head)
         self.body = nn.Sequential(*m_body)
@@ -67,27 +86,10 @@ class EDSR(nn.Module):
 
     def forward(self, x):
         x = self.head(x)
+
         res = self.body(x)
         res += x
+
         x = self.tail(res)
 
         return x
-
-    def load_state_dict(self, state_dict, strict=True):
-        own_state = self.state_dict()
-        for name, param in state_dict.items():
-            if name in own_state:
-                if isinstance(param, nn.Parameter):
-                    param = param.data
-                try:
-                    own_state[name].copy_(param)
-                except Exception:
-                    if name.find('tail') == -1:
-                        raise RuntimeError('While copying the parameter named {}, '
-                                           'whose dimensions in the model are {} and '
-                                           'whose dimensions in the checkpoint are {}.'
-                                           .format(name, own_state[name].size(), param.size()))
-            elif strict:
-                if name.find('tail') == -1:
-                    raise KeyError('unexpected key "{}" in state_dict'
-                                   .format(name))
